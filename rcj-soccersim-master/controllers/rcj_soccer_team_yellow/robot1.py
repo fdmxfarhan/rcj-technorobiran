00e935db660e1ce6f222d46be08ca3c4c769745e
Farhan Daemi <fdmxfarhan@yahoo.com>
2024-08-22 17:01:31 +0330

    Score a goal

--- rcj-soccersim-master/controllers/rcj_soccer_team_yellow/robot1.py
+++ rcj-soccersim-master/controllers/rcj_soccer_team_yellow/robot1.py
@@ -1,6 +1,9 @@
 from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
 import math, time
 
+def dist(x1, y1, x2, y2):
+    return math.sqrt((y1 - y2)**2 + (x1 - x2)**2)
+
 class MyRobot1(RCJSoccerRobot):
     def readData(self):
         pos = self.get_gps_coordinates()
@@ -29,6 +32,9 @@ class MyRobot1(RCJSoccerRobot):
         else:
             self.right_motor.setVelocity(10)
             self.left_motor.setVelocity(10)
+    def stop(self):
+        self.left_motor.setVelocity(0)
+        self.right_motor.setVelocity(0)
     def run(self):
         self.xr = 0 
         self.yr = 0
@@ -38,10 +44,27 @@ class MyRobot1(RCJSoccerRobot):
         self.ball_angle = 0
         self.ball_distance = 0
         self.is_ball = False
+        self.yt = 0
+        self.xt = 0
+        self.arrived_to_target = False
         while self.robot.step(TIME_STEP) != -1:
             self.readData()
+
+            if self.xb != 0 and self.yb != 0.75:
+                m = (self.yb - 0.75) / self.xb
+                b = 0.75
+                self.yt = self.yb - 0.1
+                self.xt = (self.yb - 0.1 - b)/m
+
             if self.is_ball:
-                self.move(self.xb, self.yb)
+                if self.arrived_to_target:
+                    self.move(self.xb, self.yb)
+                    if dist(self.xr, self.yr, self.xt, self.yt) > 0.2: 
+                        self.arrived_to_target = False
+                else:
+                    self.move(self.xt, self.yt)
+                    if dist(self.xr, self.yr, self.xt, self.yt) < 0.01: 
+                        self.arrived_to_target = True
             else:
                 self.move(0, 0)
            
