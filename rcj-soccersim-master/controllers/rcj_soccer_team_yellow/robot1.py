c48b12b096cfe21ff3b1877f0110000807ffcffb
Farhan Daemi <fdmxfarhan@yahoo.com>
2024-08-29 17:42:08 +0330

    asdf

--- rcj-soccersim-master/controllers/rcj_soccer_team_yellow/robot1.py
+++ rcj-soccersim-master/controllers/rcj_soccer_team_yellow/robot1.py
@@ -1,9 +1,14 @@
 from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
 import math, time
 
+OPONENT_GOAL_Y = 1.5
+
 def dist(x1, y1, x2, y2):
     return math.sqrt((y1 - y2)**2 + (x1 - x2)**2)
-
+def clamp(a, min_, max_):
+    if a < min_: return min_
+    if a > max_: return max_
+    return a
 class MyRobot1(RCJSoccerRobot):
     def readData(self):
         pos = self.get_gps_coordinates()
@@ -23,11 +28,28 @@ class MyRobot1(RCJSoccerRobot):
         else:
             self.is_ball = False
         ###################################### Find Behind Ball Spot
-        if self.xb != 0 and self.yb != 0.75:
-                m = (self.yb - 0.75) / self.xb
-                b = 0.75
+        if self.xb != 0 and self.yb != OPONENT_GOAL_Y:
+                m = (self.yb - OPONENT_GOAL_Y) / self.xb
+                b = OPONENT_GOAL_Y
                 self.yt = self.yb - 0.1
                 self.xt = (self.yb - 0.1 - b)/m
+        ###################################### Ersal Data be team
+        self.send_data_to_team({
+            'is_ball': self.is_ball,
+            'xb': self.xb,
+            'yb': self.yb,
+            'xr': self.xr,
+            'yr': self.yr,
+            'id': self.robot.getName()[1]
+        })
+        ###################################### Daryaft Data az team
+        while self.is_new_team_data():
+            team_data = self.get_new_team_data()['robot_id']
+            if not self.is_ball and team_data['is_ball']:
+                self.xb = team_data['xb']
+                self.yb = team_data['yb']
+                self.is_ball = True
+
     def motor(self, vl, vr):
         if vr > 10: vr = 10
         if vr <-10: vr =-10
@@ -41,8 +63,9 @@ class MyRobot1(RCJSoccerRobot):
         if dest_angle > 180: dest_angle -= 360
         if dest_angle <-180: dest_angle += 360
 
-        
-        if dest_angle > -90 and dest_angle < 90:
+        if dist(self.xr, self.yr, x, y) < 0.02 and not self.is_ball:
+            self.stop()
+        elif dest_angle > -90 and dest_angle < 90:
             self.motor(10 - dest_angle*0.4, 10 + dest_angle*0.4)
         else:
             if dest_angle >= 0: dest_angle -= 180
@@ -58,8 +81,9 @@ class MyRobot1(RCJSoccerRobot):
             else:
                 self.move(self.xb - 0.15, self.yb) 
         elif self.arrived_to_target:
-            self.move(self.xb, self.yb)
-            if dist(self.xr, self.yr, self.xt, self.yt) > 0.2: 
+            # self.move(self.xb, self.yb)
+            self.move(0 , 0.75)
+            if dist(self.xr, self.yr, self.xb, self.yb) > 0.2: 
                 self.arrived_to_target = False
         else:
             self.move(self.xt, self.yt)
@@ -73,7 +97,7 @@ class MyRobot1(RCJSoccerRobot):
         if self.robot.getName()[1] == '3':
             self.move(0.3, -0.2)
     def GoalKeeperAI(self):
-        self.move(self.xb, -0.6)
+        self.move(clamp(self.xb, -0.3, 0.3), -0.7)
     def run(self):
         self.xr = 0 
         self.yr = 0
@@ -88,6 +112,9 @@ class MyRobot1(RCJSoccerRobot):
         self.arrived_to_target = False
         while self.robot.step(TIME_STEP) != -1:
             self.readData()
+            
+
+
             if self.is_ball:
                 # self.ForwardAI()
                 self.GoalKeeperAI()
--- rcj-soccersim-master/controllers/rcj_soccer_team_yellow/robot3.py
+++ rcj-soccersim-master/controllers/rcj_soccer_team_yellow/robot3.py
@@ -1,6 +1,8 @@
 from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
 import math, time
 
+OPONENT_GOAL_Y = 1.5
+
 def dist(x1, y1, x2, y2):
     return math.sqrt((y1 - y2)**2 + (x1 - x2)**2)
 
@@ -23,11 +25,28 @@ class MyRobot3(RCJSoccerRobot):
         else:
             self.is_ball = False
         ###################################### Find Behind Ball Spot
-        if self.xb != 0 and self.yb != 0.75:
-                m = (self.yb - 0.75) / self.xb
-                b = 0.75
+        if self.xb != 0 and self.yb != OPONENT_GOAL_Y:
+                m = (self.yb - OPONENT_GOAL_Y) / self.xb
+                b = OPONENT_GOAL_Y
                 self.yt = self.yb - 0.1
                 self.xt = (self.yb - 0.1 - b)/m
+        ###################################### Ersal Data be team
+        self.send_data_to_team({
+            'is_ball': self.is_ball,
+            'xb': self.xb,
+            'yb': self.yb,
+            'xr': self.xr,
+            'yr': self.yr,
+            'id': self.robot.getName()[1]
+        })
+        ###################################### Daryaft Data az team
+        while self.is_new_team_data():
+            team_data = self.get_new_team_data()['robot_id']
+            if not self.is_ball and team_data['is_ball']:
+                self.xb = team_data['xb']
+                self.yb = team_data['yb']
+                self.is_ball = True
+
     def motor(self, vl, vr):
         if vr > 10: vr = 10
         if vr <-10: vr =-10
@@ -41,8 +60,9 @@ class MyRobot3(RCJSoccerRobot):
         if dest_angle > 180: dest_angle -= 360
         if dest_angle <-180: dest_angle += 360
 
-        
-        if dest_angle > -90 and dest_angle < 90:
+        if dist(self.xr, self.yr, x, y) < 0.02 and not self.is_ball:
+            self.stop()
+        elif dest_angle > -90 and dest_angle < 90:
             self.motor(10 - dest_angle*0.4, 10 + dest_angle*0.4)
         else:
             if dest_angle >= 0: dest_angle -= 180
@@ -58,8 +78,9 @@ class MyRobot3(RCJSoccerRobot):
             else:
                 self.move(self.xb - 0.15, self.yb) 
         elif self.arrived_to_target:
-            self.move(self.xb, self.yb)
-            if dist(self.xr, self.yr, self.xt, self.yt) > 0.2: 
+            # self.move(self.xb, self.yb)
+            self.move(0 , 0.75)
+            if dist(self.xr, self.yr, self.xb, self.yb) > 0.2: 
                 self.arrived_to_target = False
         else:
             self.move(self.xt, self.yt)
@@ -72,6 +93,8 @@ class MyRobot3(RCJSoccerRobot):
             self.move(-0.3, -0.2)
         if self.robot.getName()[1] == '3':
             self.move(0.3, -0.2)
+    def GoalKeeperAI(self):
+        self.move(self.xb, -0.6)
     def run(self):
         self.xr = 0 
         self.yr = 0
@@ -86,8 +109,12 @@ class MyRobot3(RCJSoccerRobot):
         self.arrived_to_target = False
         while self.robot.step(TIME_STEP) != -1:
             self.readData()
+            
+
+
             if self.is_ball:
                 self.ForwardAI()
+                # self.GoalKeeperAI()
             else:
                 self.Formation()
            
