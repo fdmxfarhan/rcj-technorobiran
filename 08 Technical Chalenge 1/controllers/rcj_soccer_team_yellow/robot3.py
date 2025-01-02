b87e8b0c3630e820172fae737faa853fa7c3544a
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-01-02 16:16:54 +0330

    asdf

---
+++ 08 Technical Chalenge 1/controllers/rcj_soccer_team_yellow/robot3.py
@@ -0,0 +1,35 @@
+from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
+import math, time
+from utils import *
+
+class MyRobot3(RCJSoccerRobot):
+    def ForwardAI(self):
+        if self.yr > self.yb:
+            if self.xr > self.xb:
+                move(self, self.xb + 0.15, self.yb) 
+            else:
+                move(self, self.xb - 0.15, self.yb) 
+        elif self.arrived_to_target:
+            move(self, self.xb, self.yb)
+            if dist(self.xr, self.yr, self.xb, self.yb) > 0.2: 
+                self.arrived_to_target = False
+        else:
+            move(self, self.xt, self.yt)
+            if dist(self.xr, self.yr, self.xt, self.yt) < 0.01: 
+                self.arrived_to_target = True
+    def run(self):
+        define_variables(self)
+        while self.robot.step(TIME_STEP) != -1:
+            readData(self)
+            if self.manual_control:
+                manualControl(self)
+            else:
+                if self.is_ball:
+                    if self.ball_stop_time > 3:
+                        LackOffProgress(self)
+                    else:
+                        self.ForwardAI()
+                else:
+                    Formation(self)
+           
+            
