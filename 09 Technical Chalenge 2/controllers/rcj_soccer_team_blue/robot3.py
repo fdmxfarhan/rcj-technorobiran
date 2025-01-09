f3d2d9e57e69790038ddd3c3120682e7dd4bb44d
Farhan Daemi <fdmxfarhan@yahoo.com>
2025-01-09 18:51:15 +0330

    asdf

---
+++ 09 Technical Chalenge 2/controllers/rcj_soccer_team_blue/robot3.py
@@ -0,0 +1,11 @@
+from rcj_soccer_robot import RCJSoccerRobot, TIME_STEP
+import math, time
+from utils import *
+
+class MyRobot3(RCJSoccerRobot):
+    def run(self):
+        define_variables(self)
+        while self.robot.step(TIME_STEP) != -1:
+            readData(self)
+            
+            
